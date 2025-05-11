de734c3ed063bb54006949c8dc7731708fd3562c
Daniel <github@inajiffy.eu>
2025-05-11 19:51:25 +0200

    generator config

---
+++ src/main/java/pl/wtrymiga/taskmanager/tasks/domain/TaskCodeGeneratorInMemorySequence.java
@@ -0,0 +1,11 @@
+package pl.wtrymiga.taskmanager.tasks.domain;
+
+public class TaskCodeGeneratorInMemorySequence implements TaskCodeGenerator {
+
+	@Override
+	public String nextCode(int year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
